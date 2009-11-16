c2ed9d4a92c699f2acd2cb2480f2d6f0970a399e
Jason Harrelson <jason@lookforwardenterprises.com>
2009-11-15 21:42:40 -0600

    Release 0.9.0.

--- lib/sort_it_out.rb
+++ lib/sort_it_out.rb
@@ -4,7 +4,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'sort_it_out/sortable.rb'
 
 module SortItOut
-  VERSION = '1.0.0'
+  VERSION = '0.9.0'
 end
 
-ActionController.send( :include, SortItOut::Sortable ) if defined?( ActionController )
+ActionController::Base.send( :include, SortItOut::Sortable ) if defined?( ActionController::Base )
